3c1db4b70d8a66f4e828321a40245867e44f8285
heinricov abraham <heinricoov@gmail.com>
2025-10-02 13:37:52 +0700

    develop update 13 - SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.morea.lat", // ganti dengan domain kamu
+  generateRobotsTxt: true, // otomatis bikin robots.txt
+  sitemapSize: 7000
+};
---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+import type { IConfig } from "next-sitemap";
+
+const config: IConfig = {
+  siteUrl: "https://www.morea.lat",
+  generateRobotsTxt: true,
+  sitemapSize: 7000
+};
+
+export default config;
